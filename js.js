5cbd99fd19ce9c4951770f796e027d3388e322e5
User <aidarovaidarova@gmail.com>
2024-10-04 15:21:52 +0400

    add php file and edit js

--- js.js
+++ js.js
@@ -11,3 +11,17 @@ function getRandomColor() {
   
   setInterval(changeBackgroundColor, 1000);
   
+
+  function getRandomColor() {
+      let r = Math.floor(Math.random() * 256);
+      let g = Math.floor(Math.random() * 256);
+      let b = Math.floor(Math.random() * 256);
+      return `rgb(${r}, ${g}, ${b})`;
+  }
+  
+  function changeBackgroundColor() {
+      document.body.style.backgroundColor = getRandomColor();
+  }
+  
+  setInterval(changeBackgroundColor, 1000);
+  
